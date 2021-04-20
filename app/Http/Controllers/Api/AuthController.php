e4ef96b7b70660d36bb088c64cd2a4fd53790c69
divardhia <divardhiass@gmail.com>
2021-04-20 14:56:00 +0700

    menambahkan fungsi logout pada AuthController

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use App\Traits\ApiResponse;
+use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -36,7 +37,7 @@ class AuthController extends Controller
     public function login(LoginRequest $request)
     {
         $validated = $request->validated();
-        
+
         if (!Auth::attempt($validated)){
             return $this->apiError('Credentials not match', Response::HTTP_UNAUTHORIZED);
         }
@@ -49,5 +50,16 @@ class AuthController extends Controller
             'token_type' => 'Bearer',
             'user' => $user,
         ]);
+    }
+
+    public function logout(){
+        try{
+            auth()->user()->tokens()->delete();
+            return $this->apiSuccess('Tokens revoked');
+        } catch (\Throwable $e){
+            throw new HttpResponseException($this->apiError(null,
+            Response::HTTP_INTERNAL_SERVER_ERROR
+        ));
+        }
     }
 }
