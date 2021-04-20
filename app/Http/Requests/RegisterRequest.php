c17b7f967be5100d67a75ada9b7218b708719913
divardhia <divardhiass@gmail.com>
2021-04-20 14:38:04 +0700

    memperbaiki kesalahan dan menambahkan fungsi login

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -2,9 +2,6 @@
 
 namespace App\Http\Requests;
 
-use App\Models\User;
-use Illuminate\Support\Facades\Hash;
-
 class RegisterRequest extends ApiRequest
 {
     /**
@@ -29,23 +26,5 @@ class RegisterRequest extends ApiRequest
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8'
         ];
-    }
-
-    public function register(RegisterRequest $request)
-    {
-        $validated = $request->validated();
-        $user = User::create([
-            'name' => $validated['name'],
-            'email' => $validated['email'],
-            'password' => Hash::make($validated['password']),
-        ]);
-
-        $token = $user->createToken('auth_token')->plainTextToken;
-        return $this->apiSuccess([
-            'token' => $token,
-            'token_type' => 'Bearer',
-            'user' => $user,
-        ]);
-
     }
 }
